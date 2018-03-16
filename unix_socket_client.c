b2ee4d96f03018ee76eccd88be00d997eee14122
Vladimir <vgovseychuk@gmail.com>
2018-03-16 11:29:21 +0300

    1

--- unix_socket_client.c
+++ unix_socket_client.c
@@ -14,14 +14,38 @@ LINK
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdint.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/un.h>
 
+#define NOTE_CNT_INFO		0x01
+#define DATA_TRANSFER_BYTE	0x11
 
+#define OUT_BUFF_SIZE		32
+#define IN_BUFF_SIZE		4096
 
 #define SERVER_PATH     	"/tmp/MeshDataServer"	// file name for data exchange
 #define MAX_THREAD_COUNT	5
+
+
+#pragma pack(push, 1)
+typedef struct
+{
+	uint32_t ipv4;
+	uint16_t rank;
+	uint32_t parent_ipv4;
+} node_note_t;
+
+typedef struct
+{
+	uint32_t ipv4;
+	uint32_t neighbor_ipv4;
+	uint16_t neighbor_rank;
+	int8_t   neighbor_rssi;
+	uint16_t flags;	
+} link_note_t;
+#pragma pack(pop)
 
 /*
 struct fdStructType 
@@ -48,8 +72,17 @@ int main(int argc, char **argv)
     */
 
 	int res,
-	serv_socket;
+	serv_socket,
+	read_bytes_cnt,
+	send_bytes_cnt;
 
+	uint8_t data_buffer_in[IN_BUFF_SIZE];
+	uint8_t data_buffer_out[OUT_BUFF_SIZE];
+
+	uint32_t nodes_cnt,
+	first_index, get_from,
+	last_index, get_to,
+	notes_per_msg;
 
     struct sockaddr_un server_addr;
 
@@ -70,11 +103,73 @@ int main(int argc, char **argv)
 		return res;
 	}
 
-	sleep(10);
+	sleep(1);
+
+	data_buffer_out[0] = NOTE_CNT_INFO;
+	send_bytes_cnt = 1;
+	send(serv_socket, data_buffer_out, send_bytes_cnt, 0);
 
 	while(1)
 	{
+		read_bytes_cnt = recv(serv_socket, data_buffer_in, IN_BUFF_SIZE, 0);
+		if(read_bytes_cnt) {
+			memcpy(&nodes_cnt, data_buffer_in + 1, 4);
+			printf("ANSWER: %d\n", nodes_cnt);
+			break;
+		}
+	}
+
+	sleep(1);
+
+	data_buffer_out[0] = DATA_TRANSFER_BYTE;
+	get_from = 0;
+	get_to = 499999;
+	first_index = 0;
+	last_index = -1;
+	notes_per_msg = (IN_BUFF_SIZE - 1) / sizeof(node_note_t);
+	printf("notes_per_msg: %d\n", notes_per_msg);
+	send_bytes_cnt = 9;
+
+
+
+	first_index = last_index + 1;
+	last_index = first_index + notes_per_msg - 1;
+	if(last_index > get_to) last_index = get_to;
+	printf("first_index: %d\n", first_index);
+	printf("last_index: %d\n", last_index);
+	
+
+	while(1)
+	{
+		memcpy(data_buffer_out+1, &first_index, 4);
+		memcpy(data_buffer_out+5, &last_index, 4);
+		send(serv_socket, data_buffer_out, send_bytes_cnt, 0);
+
+		usleep(100);
+
+		read_bytes_cnt = recv(serv_socket, data_buffer_in, IN_BUFF_SIZE, 0);
+
+		if(read_bytes_cnt) {
+			volatile node_note_t note;
+			for(int i = 0; i < last_index-first_index + 1; i++)
+			{
+				memcpy((uint8_t * )&note, data_buffer_in + 1 + sizeof(node_note_t)*i, sizeof(node_note_t));
+				//printf("-- NOTE #%d:\n", first_index+i);
+				//printf("ipv4: %d\n", note.ipv4);
+				/*printf("rank: %d\n", note.rank);
+				printf("parent_ipv4: %d\n", note.parent_ipv4);*/
+			}
+
+			printf("read_bytes_cnt: %d\n", read_bytes_cnt);
+			
+		}
 		
+		first_index = last_index + 1;
+		if(first_index > get_to) break;
+		last_index = first_index + notes_per_msg - 1;
+		if(last_index > get_to) last_index = get_to;
+		printf("first_index: %d\n", first_index);
+		printf("last_index: %d\n", last_index);
 	}
 
 	return 0;
--- unix_socket_server.c
+++ unix_socket_server.c
@@ -1,20 +1,7 @@
-/*
-
-Tables to move here:
-
-NODES(MESH)
-|  ID  |  IPV4  |  RANK  |  PARENT_IPV4  |
-
-
-LINK
-|  ID  |  IPV4  |  NEIGHBOR_IPV4  |  NEIGHBOR_RANK  |  NEIGHBOR_RSSI  |  FLAGS  |
-
-
-*/
-
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdint.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
@@ -22,13 +9,36 @@ LINK
 #include <sys/epoll.h>
 
 
+#define NOTE_CNT_INFO		0x01
+#define DATA_TRANSFER_BYTE	0x11
+
 #define EPOLL_TIMEOUT		-1
 #define EPOLL_EVENT_LIMIT	10
 
-#define BUFF_SIZE			256
+#define IN_BUFF_SIZE		32
+#define OUT_BUFF_SIZE		4096
 
 #define SERVER_PATH     	"/tmp/MeshDataServer"	// file name for data exchange
 #define MAX_CONN			2
+
+
+#pragma pack(push, 1)
+typedef struct
+{
+	uint32_t ipv4;
+	uint16_t rank;
+	uint32_t parent_ipv4;
+} node_note_t;
+
+typedef struct
+{
+	uint32_t ipv4;
+	uint32_t neighbor_ipv4;
+	uint16_t neighbor_rank;
+	int8_t   neighbor_rssi;
+	uint16_t flags;	
+} link_note_t;
+#pragma pack(pop)
 
 
 /******************************************
@@ -44,10 +54,34 @@ int main(int argc, char **argv) {
 	epoll_event_type, 
 	epoll_source,
 	read_bytes_cnt, 
+	send_bytes_cnt,
 	i;
 
+	const uint32_t nodes_cnt = 500000;
+	node_note_t nodes_array[nodes_cnt];
+	uint32_t first_index,
+	last_index;
+	for (int i = 0; i < nodes_cnt; ++i)
+	{
+		nodes_array[i].ipv4 = i;
+		nodes_array[i].rank = 128+i;
+		nodes_array[i].parent_ipv4 = i;
+	}
+/*
+	const uint32_t links_cnt = 500000;
+	node_note_t links_array[links_cnt];
+	for (int i = 0; i < links_cnt; ++i)
+	{
+		links_array[i].ipv4 = i;
+		links_array[i].neighbor_ipv4 = i;
+		links_array[i].neighbor_rank = i;
+		links_array[i].neighbor_rssi = i;
+		links_array[i].flags = i;
+	}
+*/
 	struct epoll_event epoll_event_arr[EPOLL_EVENT_LIMIT];
-	char data_buffer[BUFF_SIZE];
+	uint8_t data_buffer_in[IN_BUFF_SIZE];
+	uint8_t data_buffer_out[OUT_BUFF_SIZE];
 
 	struct sockaddr_un server_addr;
 
@@ -135,10 +169,45 @@ int main(int argc, char **argv) {
 			// data on existing connection
 			else if(epoll_event_type & EPOLLIN) 
 			{
-				read_bytes_cnt = recv(epoll_source, data_buffer, BUFF_SIZE, 0);
+				read_bytes_cnt = recv(epoll_source, data_buffer_in, IN_BUFF_SIZE, 0);
 				if(read_bytes_cnt > 0)
 				{
-					
+					printf("GOT: ");
+					for(int i = 0; i < read_bytes_cnt; i++)
+						printf("%02x ", *((uint8_t * )data_buffer_in+i));
+					printf("\n");
+
+
+					data_buffer_out[0] = data_buffer_in[0];
+					switch(data_buffer_in[0]){
+
+						case NOTE_CNT_INFO: 
+							memcpy(data_buffer_out+1, &nodes_cnt, 4);
+							send_bytes_cnt = 5;
+							send(epoll_source, data_buffer_out, send_bytes_cnt, 0);
+							break;
+
+						case DATA_TRANSFER_BYTE: 
+							memcpy(&first_index, data_buffer_in+1, 4);	// send notes from here
+							memcpy(&last_index,  data_buffer_in+5, 4);	// to here
+
+							send_bytes_cnt = sizeof(node_note_t)*(last_index - first_index + 1);
+
+							memcpy((uint8_t * )(data_buffer_out + 1), (const uint8_t * )(&nodes_array[first_index]), send_bytes_cnt);
+
+							send_bytes_cnt++; // including first byte(type)
+
+							printf("SEND %d NOTES (%d bytes)\n", (last_index - first_index + 1), send_bytes_cnt);
+
+							send(epoll_source, data_buffer_out, send_bytes_cnt, 0);
+							break;
+
+						default:
+							data_buffer_out[0] = 'X';
+							send_bytes_cnt = 1;
+							send(epoll_source, data_buffer_out, send_bytes_cnt, 0);
+							break;
+					}
 				}
 				else // someone wants to close connection
 				{
